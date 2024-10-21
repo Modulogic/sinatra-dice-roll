ae02df3df3523b6d426bee69aea97e42f1ac323e
Modulogic <carlomodulo@uchicago.edu>
2024-10-21 17:32:38 +0000

    erb commit

--- dice.rb
+++ dice.rb
@@ -7,21 +7,12 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-
-"
-<h1>Dice Roll</h1>
-<ul>
-<li><a href=\"dice/2/6\">Roll two 6-sided dice</a></li>
-<li><a href=\"dice/2/10\">Roll two 10-sided dice</a></li>
-<li><a href=\"dice/1/20\">Roll one 20-sided dice</a></li>
-<li><a href=\"dice/5/4\">Roll five 4-sided dice</a></li>
-</ul>
-"
+  erb(:elephant)
 end
 
  
 
-  get("/zebra") do
+get("/zebra") do
   "we must add a route for each path we want to support"
 
 end
@@ -35,10 +26,10 @@ get("/dice/2/6") do
     second_die = rand(1..6)
   sum = first_die + second_die
 
-  outcome = "you rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "you rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six)
+  
 end
 
 get("/dice/2/10") do
